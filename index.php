49549c7854245d43d5c007d1487d81daf7b4e3cf
Tayna <taynaadrianadasilva33@gmail.com>
2024-08-29 22:16:10 -0300

    atividades 29-08

---
+++ index.php
@@ -0,0 +1,23 @@
+
+<h2>Atividade 2: calculadora de idade</h2>
+<?php
+$anoNascimento = 2006;
+$anoAtual = 2024;
+$idade;
+$idade = $anoAtual - $anoNascimento;
+ echo "A idade atual é $idade";
+?>
+
+<h2>Exercício 3: Conversor de Temperatura</h2>
+<?php 
+$temperaturaCelsius = 24;
+$fahrenheit;
+$fahrenheit=($temperaturaCelsius * 9/5) + 32;
+ echo "Celsius convertido em Fahrenheit $fahrenheit";
+?>
+
+<h2>Exercício 4: Verificação de Paridade</h2>
+<?php 
+$numero = 10;
+
+?>
